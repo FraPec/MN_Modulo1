953a99b8e61ef52d3c583ade9057a38f81302c7a
FraPec <frapecorella@hotmail.it>
2024-10-23 16:09:42 +0200

    Draft of allocation and deallocation of lattice

---
+++ functions_test/lattice.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define L 5
+
+// Vector structure 
+typedef struct DoubleVector2D {
+    double sx;
+    double sy;
+} DoubleVector2D;
+
+
+void free_lattice(DoubleVector2D ***lattice, int lattice_side) {
+    int i, j;
+    for (i=0; i<lattice_side; i++) {
+        for (j=0; j<lattice_side; j++) {
+            free(lattice[i][j]);
+        }
+        free(lattice[i]);
+    }
+    free(lattice);
+}
+
+
+int allocate(DoubleVector2D ****lattice, int lattice_side) {
+    int i, j, k, l;
+    
+    // First level allocation - array of pointers (2D lattice) to array of pointers (3D lattice) to 2dVector
+    *lattice = (DoubleVector2D ***)malloc(lattice_side * sizeof(DoubleVector2D **)); 
+    if (*lattice==NULL) {
+        fprintf(stderr, "Error in the allocation of the first level.\n");
+        return EXIT_FAILURE;
+    }
+
+    // Second level allocation - array of pointers (3D lattice) to 2dVector
+    for (i=0; i<lattice_side; i++) {
+        (*lattice)[i] = (DoubleVector2D **)malloc(lattice_side * sizeof(DoubleVector2D *));
+
+        if ((*lattice)[i]==NULL) {
+            fprintf(stderr, "Error in the allocation of the second level.\n");
+            for (k=0; k<i; k++) {
+                free((*lattice)[k]);
+            }
+            free(*lattice);
+	return EXIT_FAILURE;
+        }
+        
+        // Third level allocation - pointers to 2dVector
+        for (j=0; j<lattice_side; j++) {
+            (*lattice)[i][j] = (DoubleVector2D *)malloc(lattice_side * sizeof(DoubleVector2D));
+            if ((*lattice)[i][j]==NULL) {
+            
+                fprintf(stderr, "Error in the allocation of the second level.\n");
+                
+                // Deallocation of j pointers to 2dVector along the (2D) lattice site identified by i
+                for (l=0; l<j; l++) {
+                        free((*lattice)[i][l]);
+                    }
+                free((*lattice)[i])                
+                
+                // Deallocation of (i x lattice_side) pointers to 2dVector
+                for (k=0; k<i; k++) {
+                    for (l=0; l<lattice_side; l++) {
+                        free((*lattice)[k][l]);
+                    }
+                    free((*lattice)[k]);
+                   
+                }
+                free(*lattice);
+                return EXIT_FAILURE;
+            }
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+
+int main() {
+    2D_double_vector *** lattice;
+    
+    return EXIT_SUCCESS;
+}
