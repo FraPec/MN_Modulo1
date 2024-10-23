1a8bed075883b6954902f6b78b846c5194eb2ca3
Matteo Di Sante <matteo.disante@live.it>
2024-10-23 17:50:47 +0200

    Test implementation for alloc/dealloc of lattice
    fixed allocation and deallocation of lattice and added main to test

--- functions_test/lattice.c
+++ functions_test/lattice.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
+
 #define L 5
 
-// Vector structure 
+// Vector structure
 typedef struct DoubleVector2D {
     double sx;
     double sy;
@@ -25,9 +26,9 @@ void free_lattice(DoubleVector2D ***lattice, int lattice_side) {
 
 int allocate(DoubleVector2D ****lattice, int lattice_side) {
     int i, j, k, l;
-    
+
     // First level allocation - array of pointers (2D lattice) to array of pointers (3D lattice) to 2dVector
-    *lattice = (DoubleVector2D ***)malloc(lattice_side * sizeof(DoubleVector2D **)); 
+    *lattice = (DoubleVector2D ***)malloc(lattice_side * sizeof(DoubleVector2D **));
     if (*lattice==NULL) {
         fprintf(stderr, "Error in the allocation of the first level.\n");
         return EXIT_FAILURE;
@@ -45,27 +46,27 @@ int allocate(DoubleVector2D ****lattice, int lattice_side) {
             free(*lattice);
 	return EXIT_FAILURE;
         }
-        
+
         // Third level allocation - pointers to 2dVector
         for (j=0; j<lattice_side; j++) {
             (*lattice)[i][j] = (DoubleVector2D *)malloc(lattice_side * sizeof(DoubleVector2D));
             if ((*lattice)[i][j]==NULL) {
-            
+
                 fprintf(stderr, "Error in the allocation of the second level.\n");
-                
+
                 // Deallocation of j pointers to 2dVector along the (2D) lattice site identified by i
                 for (l=0; l<j; l++) {
                         free((*lattice)[i][l]);
                     }
-                free((*lattice)[i]);         
-                
+                free((*lattice)[i]);
+
                 // Deallocation of (i x lattice_side) pointers to 2dVector
                 for (k=0; k<i; k++) {
                     for (l=0; l<lattice_side; l++) {
                         free((*lattice)[k][l]);
                     }
                     free((*lattice)[k]);
-                   
+
                 }
                 free(*lattice);
                 return EXIT_FAILURE;
@@ -76,23 +77,74 @@ int allocate(DoubleVector2D ****lattice, int lattice_side) {
 }
 
 
+
+
 int main() {
     DoubleVector2D ***lattice;
 
+
+    double a = 1.0;
+    int i, j, k;
+
+
+
     // Allocation and check if all went well
     if (allocate(&lattice, L) == EXIT_FAILURE) {
+        fprintf(stderr, "male male male/n");
         return EXIT_FAILURE;
     }
 
-    // Setting all value
-    lattice[0][0][0].sx = 1.0;
-    lattice[0][0][0].sy = 2.0;
 
-    printf("Value of lattice[0][0][0]: sx = %.2f, sy = %.2f\n", lattice[0][0][0].sx, lattice[0][0][0].sy);
+    // Setting values in the dynamically allocated lattice
+    for (i=0; i<L; i++){
+      for (j=0; j<L; j++){
+        for (k=0; k<L; k++){
+         lattice[i][j][k].sx = a;
+         lattice[i][j][k].sy = a;
+      }
+     }
+    }
 
-    // Free the lattice
-    free_lattice(lattice, L);
+
+    // test
+    int flag = 0;
+
+    for (i=0; i<L; i++){
+
+      for (j=0; j<L; j++){
+
+        for (k=0; k<L; k++){
+
+         if (lattice[i][j][k].sx != a & lattice[i][j][k].sy != a ){
+           flag = 1;
+         }
+
+         if (flag == 1) {
+           break;
+         }
+
+      }
+      if (flag == 1) {
+        break;
+      }
+
+     }
+     if (flag == 1){
+       break;
+     }
+
+    }
+
+
+    if (flag == 0)
+    { // Free the lattice
+      free_lattice(lattice, L);
+      printf("Test passed! Great job\n");
+    }
+    else {
+      printf("Test failed\n");
+    }
+
 
     return EXIT_SUCCESS;
 }
-
