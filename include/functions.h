28364569818f4dc25233b1f2d77f5870cd67ce03
Matteo Di Sante <matteo.disante@live.it>
2024-10-23 18:08:59 +0200

    added alloc and dealloc in functions.c
    Functions to allocate and deallocate lattice have been moved to functions.c. The header ../include/functions.h has been also added to functions.c. The structure to work with 2D vectors have been moved in functions.h header (structures must be defined in the header).

--- include/functions.h
+++ include/functions.h
@@ -1,9 +1,18 @@
 #ifndef FUNCTIONS
 #define FUNCTIONS
 
+
+typedef struct {
+    double sx;
+    double sy;
+} DoubleVector2D;
+
+
 int print_vector(double * x, int len_x);
 double scalar_product(double * x, double * y, int dim);
 int microcanonical(double *s, double *S, int len_s);
 int normalization(double * r, int len_r);
+void free_lattice(DoubleVector2D ***lattice, int lattice_side);
+int allocate(DoubleVector2D ****lattice, int lattice_side);
 
-#endif 
+#endif
--- lib/functions.c
+++ lib/functions.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include "../include/functions.h"
+
+
 
 int print_vector(double * x, int len_x) {
     int i;
@@ -24,7 +27,7 @@ int microcanonical(double *s, double *S, int len_s) {
     double sq_mod_S, sS_scal_prod;
     int i;
 
-    sq_mod_S = scalar_product(S, S, len_s); 
+    sq_mod_S = scalar_product(S, S, len_s);
     if (sqrt(sq_mod_S)<1e-13) {
         return EXIT_FAILURE;
     } else {
@@ -36,8 +39,8 @@ int microcanonical(double *s, double *S, int len_s) {
     }
 }
 
-int normalization(double * r, int len_r) { 
-// How many normalizations needed during a simulation? 
+int normalization(double * r, int len_r) {
+// How many normalizations needed during a simulation?
 
     int i;
     double mod_r = sqrt(scalar_product(r, r, len_r));
@@ -55,3 +58,67 @@ int normalization(double * r, int len_r) {
     }
 }
 
+
+void free_lattice(DoubleVector2D ***lattice, int lattice_side) {
+    int i, j;
+    for (i=0; i<lattice_side; i++) {
+        for (j=0; j<lattice_side; j++) {
+            free(lattice[i][j]);
+        }
+        free(lattice[i]);
+    }
+    free(lattice);
+}
+
+
+int allocate(DoubleVector2D ****lattice, int lattice_side) {
+    int i, j, k, l;
+
+    // First level allocation - array of pointers (2D lattice) to array of pointers (3D lattice) to 2dVector
+    *lattice = (DoubleVector2D ***)malloc(lattice_side * sizeof(DoubleVector2D **));
+    if (*lattice==NULL) {
+        fprintf(stderr, "Error in the allocation of the first level.\n");
+        return EXIT_FAILURE;
+    }
+
+    // Second level allocation - array of pointers (3D lattice) to 2dVector
+    for (i=0; i<lattice_side; i++) {
+        (*lattice)[i] = (DoubleVector2D **)malloc(lattice_side * sizeof(DoubleVector2D *));
+
+        if ((*lattice)[i]==NULL) {
+            fprintf(stderr, "Error in the allocation of the second level.\n");
+            for (k=0; k<i; k++) {
+                free((*lattice)[k]);
+            }
+            free(*lattice);
+	return EXIT_FAILURE;
+        }
+
+        // Third level allocation - pointers to 2dVector
+        for (j=0; j<lattice_side; j++) {
+            (*lattice)[i][j] = (DoubleVector2D *)malloc(lattice_side * sizeof(DoubleVector2D));
+            if ((*lattice)[i][j]==NULL) {
+
+                fprintf(stderr, "Error in the allocation of the second level.\n");
+
+                // Deallocation of j pointers to 2dVector along the (2D) lattice site identified by i
+                for (l=0; l<j; l++) {
+                        free((*lattice)[i][l]);
+                    }
+                free((*lattice)[i]);
+
+                // Deallocation of (i x lattice_side) pointers to 2dVector
+                for (k=0; k<i; k++) {
+                    for (l=0; l<lattice_side; l++) {
+                        free((*lattice)[k][l]);
+                    }
+                    free((*lattice)[k]);
+
+                }
+                free(*lattice);
+                return EXIT_FAILURE;
+            }
+        }
+    }
+    return EXIT_SUCCESS;
+}
