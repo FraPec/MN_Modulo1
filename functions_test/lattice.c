38b7fe5f42b184c008a38370a13b60befe769864
Matteo Di Sante <matteo.disante@live.it>
2024-10-25 13:27:14 +0200

    modified allocate function

--- functions_test/lattice.c
+++ functions_test/lattice.c
@@ -4,7 +4,9 @@
 #include "../include/functions.h"
 
 
-#define L 5
+#define L 5 // Define the size of the lattice
+
+
 
 
 
@@ -13,50 +15,52 @@ int main() {
     double a = 1.0;
     int i, j, k;
 
-    // Allocation and check if all went well
-    if (allocate(&lattice, L) == EXIT_FAILURE) {
-        fprintf(stderr, "male male male/n");
+    // Allocate the lattice and check if allocation was successful
+    lattice = allocate(L);
+    if (lattice == NULL) {
+        fprintf(stderr, "Error: Allocation of lattice failed.\n");
         return EXIT_FAILURE;
     }
 
-    // Setting values in the dynamically allocated lattice
-    for (i=0; i<L; i++) {
-        for (j=0; j<L; j++) {
-            for (k=0; k<L; k++) {
-                lattice[i][j][k].sx = a;
-                lattice[i][j][k].sy = a;
+    // Initialize values in the dynamically allocated lattice
+    for (i = 0; i < L; i++) {
+        for (j = 0; j < L; j++) {
+            for (k = 0; k < L; k++) {
+                lattice[i][j][k].sx = a;  // Set sx to 1.0
+                lattice[i][j][k].sy = a;  // Set sy to 1.0
             }
         }
     }
 
-    // test
-    int flag = 0;
-    for (i=0; i<L; i++) {
-        for (j=0; j<L; j++) {
-            for (k=0; k<L; k++) {
-                if (lattice[i][j][k].sx != a & lattice[i][j][k].sy != a ) {
-                flag = 1;
-                }
-                if (flag == 1) {
+    // Test: Verify that each element in lattice has sx = 1.0 and sy = 1.0
+    int flag = 0;  // Flag to track if any element is incorrect
+    for (i = 0; i < L; i++) {
+        for (j = 0; j < L; j++) {
+            for (k = 0; k < L; k++) {
+                // Check if either sx or sy does not match the expected value
+                if (lattice[i][j][k].sx != a || lattice[i][j][k].sy != a) {
+                    flag = 1;  // Set flag if there's a mismatch
                     break;
                 }
             }
             if (flag == 1) {
                 break;
             }
-
         }
         if (flag == 1) {
             break;
         }
     }
 
-    if (flag == 0) { // Free the lattice
-      free_lattice(lattice, L);
-      printf("Test passed! Great job\n");
+    // Output result of the test
+    if (flag == 0) {
+        printf("Test passed! All elements in lattice initialized correctly.\n");
     } else {
-        printf("Test failed\n");
+        printf("Test failed: Some elements in lattice do not match the expected values.\n");
     }
+
+    // Free the lattice
+    free_lattice(lattice, L);
 
     return EXIT_SUCCESS;
 }
--- include/functions.h
+++ include/functions.h
@@ -11,6 +11,6 @@ double scalar_product(DoubleVector2D s1, DoubleVector2D s2);
 int microcanonical(double *s, double *S, int len_s);
 int normalization(double * r, int len_r);
 void free_lattice(DoubleVector2D ***lattice, int lattice_side);
-int allocate(DoubleVector2D ****lattice, int lattice_side);
+DoubleVector2D ***allocate(int lattice_side);
 
 #endif
