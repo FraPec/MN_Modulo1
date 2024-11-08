7c41fc691190cf828544197e28e9c27b7d3c07d9
FraPec <frapecorella@hotmail.it>
2024-11-08 15:29:54 +0100

    Added compile.sh
    compile.sh is a file to autocompile using all the needed libraries and all the useful optimizations. Its usage is ./compile.sh file_to_compile.c and will output an executable file with the name file_to_compile.o

---
+++ src/compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if a file was provided as an argument
+if [ -z "$1" ]; then
+    echo "Usage: ./compile.sh file_to_compile.c"
+    exit 1
+fi
+
+# Get the filename without the extension
+filename="${1%.*}"
+
+# Compile the file with optimization flags and all the useful libraries
+gcc -o "$filename".o "$1" ../lib/functions.c ../lib/random.c ../lib/pcg32min.c -O3 -march=native -mtune=native -flto -funroll-loops -fstrict-aliasing -ffast-math -lm
+
+# Check if the compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Compilation successful: $filename"
+else
+    echo "Compilation failed."
+    exit 1
+fi
+
