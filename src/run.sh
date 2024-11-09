d6808d4eceb2522e6e6f7d21b8545b4e66d6eb46
Matteo Di Sante <matteo.disante@live.it>
2024-11-09 12:46:42 +0100

    modified run.sh
    Added a check for directories existence. Added sub directories for different lattice sides. Shorted file names to improve readability

--- src/run.sh
+++ src/run.sh
@@ -1,33 +1,45 @@
 #!/bin/bash
 
 # Define the range of each parameter
-beta_values=(0.3 0.4) #(0.3 0.4 0.5 0.6)          
-alpha_values=0.2 #(0.001 0.01 0.1 1.0)        
-lattice_side_values=20 #(20 30 40)     
+beta_values=(0.3)# 0.4 0.5 0.6)
+alpha_values=(0.1)
+lattice_side_values=(40) # 30 40)
 
 # Get the number of available processors
-num_procs=2
+num_procs=8
 
 # Counter to track the number of running processes
 proc_count=0
 
 # Create directories for input, data, and output files
-mkdir -p inputs data outputs
+# Check and remove directories if they exist, reindirizzando stdout e stderr
+[[ -d inputs ]] && rm -r inputs
+[[ -d data ]] && rm -r data
+[[ -d outputs ]] && rm -r outputs
+mkdir data outputs
+
+
+for lattice_side in "${lattice_side_values[@]}"; do
+    mkdir -p inputs/lattice${lattice_side}
+    mkdir -p data/lattice${lattice_side}
+    mkdir -p outputs/lattice${lattice_side}
+done
+
 
 # Loop over each combination of beta, alpha, and lattice_side
 for beta in "${beta_values[@]}"; do
     for alpha in "${alpha_values[@]}"; do
         for lattice_side in "${lattice_side_values[@]}"; do
-            
+
             # Define filenames within their respective folders
-            input_file="inputs/input_beta${beta}_alpha${alpha}_lattice${lattice_side}.in"
-            data_file="data/data_beta${beta}_alpha${alpha}_lattice${lattice_side}.dat"
-            output_file="outputs/output_beta${beta}_alpha${alpha}_lattice${lattice_side}.out"
+            input_file="inputs/lattice${lattice_side}/input_b${beta}_a${alpha}_L${lattice_side}.in"
+            data_file="data/lattice${lattice_side}/data_b${beta}_a${alpha}_L${lattice_side}.dat"
+            output_file="outputs/lattice${lattice_side}/output_b${beta}_a${alpha}_L${lattice_side}.out"
 
             # Generate the input file for this run
             cat > "$input_file" <<EOF
 lattice_side $lattice_side
-sample 100000    # Adjust the number of steps as desired
+sample 1000000    # Adjust the number of steps as desired
 beta $beta
 alpha $alpha
 epsilon 0.2    # Adjust epsilon as needed
@@ -52,5 +64,3 @@ done
 # Final wait to ensure all background processes complete
 wait
 echo "All simulations have completed."
-
-
